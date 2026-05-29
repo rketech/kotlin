cbcc5610263b762df73c3dfe0c78079d1722307a
rketech <122847077+rketech@users.noreply.github.com>
2026-05-29 13:53:39 +0530

    // We created a Kotlin file called Main.kt, and we used the following code to print "Hello World" to the screen // The fun keyword is used to declare a function. // A function is a block of code designed to perform a particular task. // In the example below, it declares the main() function. // The main() function is something you will see in every Kotlin program. // This function is used to execute code. // Any code inside the main() function's curly brackets {} will be executed. // For example, the println() function is inside the main() function, meaning that this will be executed. // The println() function is used to output/print text, and in our example it will output "Hello World".

---
+++ src/Main.kt
@@ -0,0 +1,13 @@
+// We created a Kotlin file called Main.kt, and we used the following code to print "Hello World" to the screen
+// The fun keyword is used to declare a function.
+// A function is a block of code designed to perform a particular task.
+// In the example below, it declares the main() function.
+// The main() function is something you will see in every Kotlin program.
+// This function is used to execute code.
+// Any code inside the main() function's curly brackets {} will be executed.
+// For example, the println() function is inside the main() function, meaning that this will be executed.
+// The println() function is used to output/print text, and in our example it will output "Hello World".
+
+fun main(){
+    println("Hello World")
+}
