8eadd64433af49208103562de866ed37d17741d8
rketech <122847077+rketech@users.noreply.github.com>
2026-05-29 14:30:39 +0530

    print function

---
+++ src/Output.kt
@@ -0,0 +1,17 @@
+fun main() {
+    // The println() function is used to output values/print text
+    println("Hello World!")
+
+    // You can add as many println() functions as you want. Note that it will add a new line for each function
+    println("Hello World!")
+    println("I am learning Kotlin.")
+    println("It is awesome!")
+
+    // You can also print numbers, and perform mathematical calculations
+    println(2 + 3)
+
+    // There is also a print() function, which is similar to println(). The only difference is that it does not insert a new line at the end of the output
+    print("Hello World! ")
+    print("I am learning Kotlin. ")
+    print("It is awesome!")
+}
