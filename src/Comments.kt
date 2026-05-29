c81eaff89d80a0a4b2b9232e4b3bf004a47295b0
rketech <122847077+rketech@users.noreply.github.com>
2026-05-29 15:14:33 +0530

    Comments

---
+++ src/Comments.kt
@@ -0,0 +1,16 @@
+// Kotlin Comments
+// Comments can be used to explain Kotlin code, and to make it more readable.
+// It can also be used to prevent execution when testing alternative code.
+
+fun main() {
+    // Single-line comments starts with two forward slashes (//).
+    //Any text between // and the end of the line is ignored by Kotlin (will not be executed).
+    println("Hello World! Single-line comments ") // This is a comment
+
+    /* Multi-line comments start with /* and ends with */.
+    Any text between /* and */ will be ignored by Kotlin. */
+
+    /*  The code below will print the words Hello World
+        to the screen, and it is amazing */
+    println("Hello World! Multi-line comments")
+}
