00dc8b96f38b49f3b35a39077a7af2c2327b360c
rketech <122847077+rketech@users.noreply.github.com>
2026-06-06 12:13:48 +0530

    For Loop

--- src/ForLoop.kt
+++ src/ForLoop.kt
@@ -4,9 +4,21 @@
         To loop through array elements, use the for loop together with the in operator:
 */
 
-fun main(){
-    val cars = listOf("Maruti","Volvo","BMW","Audi","Mercedes")
-    for(car in cars){
+fun main() {
+    val cars = listOf("Maruti", "Volvo", "BMW", "Audi", "Mercedes")
+    for (car in cars) {
         println(car)
+
+
+    }
+
+    /*
+        You can loop through all kinds of arrays. In the example above, we used an array of strings.
+        In the example below, we loop through an array of integers:
+    */
+
+    val points = mutableListOf(1, 2, 3, 4, 5)
+    for (x in points) {
+        println(x)
     }
 }
