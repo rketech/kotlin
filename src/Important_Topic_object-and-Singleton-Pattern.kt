456d4f274f7909a7f9c077483fc57ac455fb4aca
rketech <122847077+rketech@users.noreply.github.com>
2026-06-11 13:12:58 +0530

    IMP - object and Singleton Pattern

--- src/Important_Topic_object-and-Singleton-Pattern.kt
+++ src/Important_Topic_object-and-Singleton-Pattern.kt
@@ -1,3 +1,5 @@
+import UserManager.currentUser
+
 /*
     ---------- object and Singleton Pattern -----------
     Kotlin topic that gives the biggest Android payoff is:
@@ -47,15 +49,65 @@
         An object creates a single instance automatically.
 */
 
-    object MyObject{
-        var name = "Rakesh"
-    }
+object MyObject // Creates a Singleton Object named MyObject . Meaning Only one instance exists.
+{
+    var name = "Rakesh"
+}
 
-    fun main(){
-        println(MyObject.name)
+object Calculator // Creates a Singleton Object named Calculator . Meaning Only one instance exists.
+{
+    fun add(a: Int, b: Int): Int {
+        return a + b
+    }
+}
+
+// const means: This value is known at compile time and will never change.
+object Constants // creates a singleton object named Constants
+{
+    const val BASE_URL = "https://api.example.com"
+    const val APP_NAME = "My App"
+}
+
+object Counter {
+    var count = 0
+
+    fun increment() {
+        count++
+    }
+}
+
+object UserManager
+{
+    /* private set is a Kotlin feature that allows:
+            Everyone can READ the value
+            But only the class/object itself can CHANGE the value */
+    var currentUser = "Guest"
+        private set // ➡ Read from anywhere ✅ ➡ Write only inside class/object ✅
+    fun login(name: String) {
+        currentUser = name
+    }
+}
+
+fun main() {
+    println(MyObject.name)
+
+    println(Calculator.add(2, 3)) // We never created Calculator() , we directly used Calculator.add()
+
+    println(Constants.APP_NAME) // directly accesses its (Constants) property.
+
+    Counter.increment() // Directly accessing the function
+    Counter.increment() // Directly accessing the function
+
+    println(Counter.count) // Directly accessing the property
+
+    UserManager.login("Rakesh")
+    //currentUser = "Ravi" // Operation NOT Allowed as currentUser is private set
+    println("Current User : ${UserManager.currentUser}")
+
 }
 
 /*
+-----------------------------
     Think of it as
 
     With a class:
@@ -80,5 +132,63 @@
         StudentManager
 
     for the whole app.
+
+    ----------------------------
+
+        Mental Model :
+        Class
+            Blueprint
+
+                class Student
+
+            Create objects:
+
+                val s1 = Student()
+                val s2 = Student()
+
+            Multiple instances.
+
+        Object
+
+            Singleton
+
+                object StudentManager
+
+        Use directly:
+
+                StudentManager
+
+        Only one instance exists.
+
+        ---------------------------
+        ----Important-------------
+        Why Android Developers Use object
+
+        Imagine you have app-wide settings:
+
+            object AppSettings {
+                var isDarkMode = false
+            }
+
+        From anywhere in the app:
+
+            AppSettings.isDarkMode = true
+
+        and later:
+
+            println(AppSettings.isDarkMode)
+
+        You get the same value because there is only one instance.
+
+        ------------------------------
+
+        Common Android uses
+            object Constants
+            object NetworkManager
+            object DatabaseProvider
+            object SessionManager
+            object AppSettings
+
+        You'll encounter these frequently when building Android apps.
 */
 
