4a14cc2514e5c02cfb80b26277eabe9211dc734c
rketech <122847077+rketech@users.noreply.github.com>
2026-08-22 09:07:56 +0530

    since copy() work on data class, I created a data class named Student() / Then created an object name student_1 from the Student class and provided values to its properties or specific property / NOTE: map() copy() leave the original list as it it. It just creates a new copy of the list and performs the operation on to it

--- src/collectionFindCopyMapRKMartAndroid.kt
+++ src/collectionFindCopyMapRKMartAndroid.kt
@@ -42,11 +42,38 @@ fun main() {
     val number_2 = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 
     val newNumbers_2 = number_2.map { number ->
-        if (number == 2){
+        if (number == 2) {
             number + 1
-        }else{
+        } else {
             number
         }
     }
     println("New numbers after map() keeping every number in original except 2 is : $newNumbers_2")
+
+    /*
+    copy()
+        Now we need to understand copy().
+        copy() is particularly useful with a Kotlin data class.
+     */
+
+    data class Student(
+        val name: String,
+        val age: Int
+    )
+
+    val student_1 = Student(
+        "John",
+        18
+    )
+
+    println("Old Age : ${student_1.name}'s age is ${student_1.age}")
+
+    // Now using copy() which works on data class we will be increasing the age by 1
+    // New virtual list is created and changes are applied on that new list
+    val student_2 = student_1.copy(age = 19)
+
+    println("New Age : ${student_2.name}'s age is ${student_2.age}")
+    println("            But")
+    // Old List is unchanged.
+    println("Old Age is still: ${student_1.name}'s age is ${student_1.age}")
 }
