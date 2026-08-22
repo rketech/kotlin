63ce0677dfa7e56ddfe56c9ebdb70ca802854661
rketech <122847077+rketech@users.noreply.github.com>
2026-08-22 08:35:29 +0530

    Created a new listOf() numbers and assigned it to a variable / Then to perform map() operation on a specific item in the list we created a lamda expression and applied the if..else.. condition

--- src/collectionFindCopyMapRKMartAndroid.kt
+++ src/collectionFindCopyMapRKMartAndroid.kt
@@ -1,5 +1,11 @@
 fun main() {
+    /*
+    What is a Collection?
+        A collection is simply a way of keeping multiple values together.
+    */
+
     // Creating a collection listOf() fruits to perform the find() operation on to it
+    // What happens if find() doesn't find anything? result = null
     val fruits = listOf(
         "Apple",
         "Banana",
@@ -18,15 +24,29 @@ fun main() {
     println("Result after find() is : $result")
 
     // Creating a collection listOf() numbers to perform the map() operation on to it
-    val numbers = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    val numbers_1 = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 
     // I want to create a new list where every number is increased by 1.
-    val newNumbers = numbers.map { number -> number + 1 }
+    val newNumbers_1 = numbers_1.map { number -> number + 1 }
 
     /*
     Important distinction
         map() does not mean "find something."
     It means:
         Go through every item and produce a corresponding value for the new list.*/
-    println("New numbers after map() is : $newNumbers")
+    println("New numbers after map() is : $newNumbers_1")
+
+    // map() can also leave something unchanged
+    // I say: "Only increase 2. Leave everything else alone."
+
+    val number_2 = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+    val newNumbers_2 = number_2.map { number ->
+        if (number == 2){
+            number + 1
+        }else{
+            number
+        }
+    }
+    println("New numbers after map() keeping every number in original except 2 is : $newNumbers_2")
 }
