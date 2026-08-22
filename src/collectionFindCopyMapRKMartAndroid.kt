6b7d2fca8bf22925960bf5a09de9595cbe0fd3a3
rketech <122847077+rketech@users.noreply.github.com>
2026-08-22 08:08:35 +0530

    Created a collection using listOf() and stored numbers / Next applied map() on that listOf() which was assigned to a variable which then created a new list and increased each value means item by 1

--- src/collectionFindCopyMapRKMartAndroid.kt
+++ src/collectionFindCopyMapRKMartAndroid.kt
@@ -1,5 +1,5 @@
 fun main() {
-// Creating a collection listOf() fruits to perform the find() operation on to it
+    // Creating a collection listOf() fruits to perform the find() operation on to it
     val fruits = listOf(
         "Apple",
         "Banana",
@@ -16,4 +16,17 @@ fun main() {
     //val result = fruits.find { it == "Pear" }
 
     println("Result after find() is : $result")
+
+    // Creating a collection listOf() numbers to perform the map() operation on to it
+    val numbers = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+    // I want to create a new list where every number is increased by 1.
+    val newNumbers = numbers.map { number -> number + 1 }
+
+    /*
+    Important distinction
+        map() does not mean "find something."
+    It means:
+        Go through every item and produce a corresponding value for the new list.*/
+    println("New numbers after map() is : $newNumbers")
 }
